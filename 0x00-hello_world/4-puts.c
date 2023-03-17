2d05a6bad2443b7f2d0d52b1a928e62a6c5b2066
fatima <ag3932160@gmail.com>
2023-03-16 19:48:54 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main -using puts function
  *
  * Return: 0 (Success)
  */
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - using the printf function
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
