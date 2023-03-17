1d7a323d543b642fee1946c68fd351380e1bf1bd
fatima <ag3932160@gmail.com>
2023-03-16 19:28:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+/**
+ * puts command*/
 int main() {
-	puts ("Programming is like building a multilingual puzzle);
+	puts ("\"Programming is like building a multilingual puzzle)";
 	return 0;
-]
+}
