56b108fb61d009a221b0dfdff91d44e47159d725
fatima <ag3932160@gmail.com>
2023-03-22 18:01:08 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * print_times_table - prints the n times table, starting with 0
  *
@@ -19,24 +20,21 @@
 					if (j == 0)
 					{
 						_putchar(k + '0');
-					}
-					else if (k < 10 && j != 0)
+					} else if (k < 10 && j != 0)
 					{
 						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar(k + '0');
-					}
-					else if (k >= 10 && k < 100)
+					} else if (k >= 10 && k < 100)
 					{
 						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar((k / 10) + '0');
 						_putchar((k % 10) + '0');
-					}
-					else if (k >= 100)
+					} else if (k >= 100)
 					{
 						_putchar(',');
 						_putchar(' ');
