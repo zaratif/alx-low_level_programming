0185a19d9daa85916a3057b05375ddd4277f32d3
fatima <ag3932160@gmail.com>
2023-04-03 19:09:39 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@
  */
 char *_memcpy(char *dest, const char *src, unsigned int n)
 {
-	unsigned int i;
+    unsigned int i;
 
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+    for (i = 0; i < n; i++)
+        dest[i] = src[i];
 
-	return (dest);
+    return (dest);
 }
