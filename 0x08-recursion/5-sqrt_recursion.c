3b01d4321ceaff0c51b31f35abecd5a1fc28361d
fatima <ag3932160@gmail.com>
2023-04-05 20:02:46 -0700

    tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,7 +16,7 @@ int find_sqrt(int num, int root)
 	if (root == num / 2)
 		return (-1);
 
-	return (find_sqrt(num, root + 1)):
+	return (find_sqrt(num, root + 1));
 }
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
