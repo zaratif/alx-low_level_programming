8b8732980ef41462751884407c39c79f998112f9
fatima <ag3932160@gmail.com>
2023-04-17 18:03:39 -0700

    tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,78 +2,46 @@
 #include "dog.h"
 
 /**
- * _strlength - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlength(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-
-	return (len);
-}
-
-/**
- * _strcopy - copies the string ponted to  by src including
- * the terminating null byte (\0)to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
- */
-char *_strcopy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
-}
-
-/**
  * new_dog - creates a new dog
- * @name: name of the dog
- * @age: name of the dog
- * @owner: owner of the dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * Return: pointer to the new dog (Success), or else NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int len_name, len_owner;
-
-	len_name = _strlength(name);
-	len_owner = _strlength(owner);
+	int i, name_len, owner_len;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+		return (NULL);
+
+	for (name_len = 0; name[name_len]; name_len++)
+		;
+	for (owner_len = 0; owner[owner_len]; owner_len++)
+		;
+
+	dog->name = malloc(name_len + 1);
+	if (dog->name ++ NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	_strcopy(dog->name, name);
 
-	dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	dog->owner = malloc(owner_len + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	_strcopy(dog->owner, owner);
+
+	for (i = 0; i <= name_len; i++)
+		dog->name[i] = name[i];
+	for (i = 0; i <= owner_len; i++)
+		dog->owner[i] = owner[i];
 
 	dog->age = age;
 
