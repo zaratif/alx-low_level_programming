79194ced6c28479c8ba85ee7afd497b4a2d00ede
fatima <ag3932160@gmail.com>
2023-04-17 17:49:07 -0700

    tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,9 +20,8 @@ int _strlength(char *s)
 }
 
 /**
- * _strcopy - copies the string ponted to  by src
- * including the terminating null byte (\0)
- * to the bffer pointed to by dest
+ * _strcopy - copies the string ponted to  by src including
+ * the terminating null byte (\0)to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
  *
@@ -44,39 +43,39 @@ char *_strcopy(char *dest, char *src)
 }
 
 /**
- * create_dog - creates a new dog
+ * new_dog - creates a new dog
  * @name: name of the dog
  * @age: name of the dog
  * @owner: owner of the dog
  *
  * Return: pointer to the new dog (Success), NULL otherwise
  */
-dog_t *create_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *dog;
 	int len_name, len_owner;
 
 	len_name = _strlength(name);
 	len_owner = _strlength(owner);
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
 	{
-		free(new_dog);
+		free(dog);
 		return (NULL);
 	}
-	_strcopy(new_dog->name, name);
+	_strcopy(dog->name, name);
 
-	new_dog->owner = malloc(sizeof(char) * (len_owner + 1));
-	if (new_dog->owner == NULL)
+	dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (dog->owner == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog);
+		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
-	_strcopy(new_dog->owner, owner);
+	_strcopy(dog->owner, owner);
 
-	new_dog->age = age;
+	dog->age = age;
 
-	return (new_dog);
+	return (dog);
 }
